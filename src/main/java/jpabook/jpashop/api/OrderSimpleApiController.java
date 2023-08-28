70f9e7cd4f5eeaec5fe05bd128241726e6b0c1c9
opixxx <opix0306@naver.com>
2023-08-28 14:35:10 +0900

    fetch join

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -46,6 +46,20 @@ public class OrderSimpleApiController {
                 .collect(Collectors.toList());
         return result;
     }
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+    @GetMapping("/api/v4/simple-orders")
+    public List<SimpleOrderDto> ordersV4() {
+
+    }
+
 
     @Data
     static class SimpleOrderDto {
