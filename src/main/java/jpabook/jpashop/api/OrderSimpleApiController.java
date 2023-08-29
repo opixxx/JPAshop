799eb0e2b95b74a6c274eaaaaba0aa15429e8afb
opixxx <opix0306@naver.com>
2023-08-29 23:09:32 +0900

    엔티티 직접 노출

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -31,10 +31,10 @@ public class OrderSimpleApiController {
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
-//        for (Order order : all) {
-//            order.getMember().getName(); //LAZY 강제 초기화
-//            order.getDelivery().getAddress(); //LAZY 강제 초기화
-//        }
+        for (Order order : all) {
+            order.getMember().getName(); //LAZY 강제 초기화
+            order.getDelivery().getAddress(); //LAZY 강제 초기화
+        }
         return all;
     }
 
