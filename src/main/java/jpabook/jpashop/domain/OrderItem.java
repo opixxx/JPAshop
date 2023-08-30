80b3ca3815da20a4f5bb110fee93daa8899bb299
opixxx <opix0306@naver.com>
2023-08-30 15:43:49 +0900

    컬렉션 조회(엔티티 직접 노출)

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -20,11 +20,12 @@ public class OrderItem {
     @Column(name = "order_item_id")
     private Long id;
 
-    @JsonIgnore
+
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "item_id")
     private Item item;
 
+    @JsonIgnore
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
