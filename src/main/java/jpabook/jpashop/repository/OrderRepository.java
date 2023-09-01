a04ffbb286d0deff0f2f09fd41b3b921f465733b
opixxx <opix0306@naver.com>
2023-09-01 13:38:33 +0900

    컬렉션 조회(JPA 에서 DTO 직접 조회)

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -20,9 +20,7 @@ public class OrderRepository {
 
     private final EntityManager em;
 
-    public void save(Order order) {
-        em.persist(order);
-    }
+    public void save(Order order) { em.persist(order); }
 
     public Order findOne(Long id) {
         return em.find(Order.class, id);
